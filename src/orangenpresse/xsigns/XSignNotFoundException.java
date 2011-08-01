9121ab058430f171df4a735d11a8e7b6e604b6d7
orangenpresse <Benedict@Benedict-Burckharts-MacBook-Pro.local>
2011-08-01 12:52:18 +0200

    restructured the code to add the features

---
+++ src/orangenpresse/xsigns/XSignNotFoundException.java
@@ -0,0 +1,10 @@
+package orangenpresse.xsigns;
+
+public class XSignNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	XSignNotFoundException(){};
+}
